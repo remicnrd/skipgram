45b97473509e3868a8159a77b00b7f0d3ea84d32
remicnrd <remi.cnrd@gmail.com>
2018-02-11 22:41:45 +0100

    Initial commit

---
+++ skipGram.py
@@ -0,0 +1,72 @@
+from __future__ import division
+import argparse
+import pandas as pd
+
+# useful stuff
+import numpy as np
+from scipy.special import expit
+from sklearn.preprocessing import normalize
+
+
+__authors__ = ['author1','author2','author3']
+__emails__  = ['fatherchristmoas@northpole.dk','toothfairy@blackforest.no','easterbunny@greenfield.de']
+
+def text2sentences(path):
+    # feel free to make a better tokenization/pre-processing
+    sentences = []
+    with open(path) as f:
+        for l in f:
+            sentences.append( l.lower().split() )
+    return sentences
+
+def loadPairs(path):
+    data = pd.read_csv(path,delimiter='\t')
+    pairs = zip(data['word1'],data['word2'],data['similarity'])
+    return pairs
+
+
+class mSkipGram:
+    def __init__(self,sentences, nEmbed=100, negativeRate=5, winSize = 5, minCount = 5):
+        raise NotImplementedError('implement it!')
+
+
+    def train(self,stepsize, epochs):
+        raise NotImplementedError('implement it!')
+
+    def save(self,path):
+        raise NotImplementedError('implement it!')
+
+    def similarity(self,word1,word2):
+        """
+            computes similiarity between the two words. unknown words are mapped to one common vector
+        :param word1:
+        :param word2:
+        :return: a float \in [0,1] indicating the similarity (the higher the more similar)
+        """
+        raise NotImplementedError('implement it!')
+
+    @staticmethod
+    def load(path):
+        raise NotImplementedError('implement it!')
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--text', help='path containing training data', required=True)
+    parser.add_argument('--model', help='path to store/read model (when training/testing)', required=True)
+    parser.add_argument('--test', help='enters test mode', action='store_true')
+
+    opts = parser.parse_args()
+
+    if not opts.test:
+        sentences = text2sentences(opts.text)
+        sg = mySkipGram(sentences)
+        sg.train()
+        sg.save(opts.model)
+
+    else:
+        pairs = loadPairs(opts.text)
+
+        sg = mSkipGram.load(opts.model)
+        for a,b,_ in pairs:
+            print sg.similarity(a,b)
